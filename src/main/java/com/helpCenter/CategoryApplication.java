63fbb863368051de7b3c2ff8f75d9c20436d381b
guptasourya <121170479+guptasourya@users.noreply.github.com>
2023-03-30 14:27:13 +0530

    Done some changes

--- src/main/java/com/helpCenter/CategoryApplication.java
+++ src/main/java/com/helpCenter/CategoryApplication.java
@@ -1,42 +1,24 @@
 package com.helpCenter;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.helpCenter.user.entity.Constants;
-import com.helpCenter.user.entity.Role;
-import com.helpCenter.user.repository.RoleRepository;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
-public class CategoryApplication implements CommandLineRunner {
-
-	@Autowired
-	private RoleRepository roleRepository;
+public class CategoryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CategoryApplication.class, args);
 	}
+
 	@Override
-	public void run(String... args) throws Exception {
-		Role role1 = new Role();
-		role1.setRoleId(Constants.ROLE_ADMIN);
-		role1.setRole("ROLE_ADMIN");
-
-		Role role2 = new Role();
-		role2.setRoleId(Constants.ROLE_NORMAL);
-		role2.setRole("ROLE_NORMAL");
-
-		List<Role> roles = List.of(role1, role2);
-		roleRepository.saveAll(roles);
-
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CategoryApplication.class);
 	}
 
 }
