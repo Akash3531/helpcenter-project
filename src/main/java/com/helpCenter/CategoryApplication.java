1fc208bb470a027362e891a6dc64c18312b53d52
Akash <dingramail000@gmail.com>
2023-04-10 15:04:12 +0530

    chages

--- src/main/java/com/helpCenter/CategoryApplication.java
+++ src/main/java/com/helpCenter/CategoryApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.helpCenter.notificationsEmails.scheduler.SchedulerForCategoryEtaExpiration;
-
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
